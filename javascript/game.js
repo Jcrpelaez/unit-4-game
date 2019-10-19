a9569457bc712435426175e379f8308f14915868
Juan Carlos <jcrpelaez@yahoo.com>
2019-10-18 22:14:22 -0500

    added random values for stones

--- javascript/game.js
+++ javascript/game.js
@@ -1,49 +1,48 @@
 // Created Variables for counters
-var targetScore  = 0;
-var wins         = 0;
-var losses       = 0;
-var currentScore = 0; 
+var targetScore = 0;
+var wins = 0;
+var losses = 0;
+var currentScore = 0;
 
 // Created values for each stone
-var blue
-var yellow
-var green
-var purple
+var blue = Math.floor(Math.random() * 12) + 1;
+var yellow = Math.floor(Math.random() * 12) + 1;
+var green = Math.floor(Math.random() * 12) + 1;
+var purple = Math.floor(Math.random() * 12) + 1;
 
 // Function to begin the game
 
-var startGame = function(){
-    var currentScore = 0;
+var startGame = function() {
+  var currentScore = 0;
+
+  targetScore = Math.floor(Math.random() * 120) + 19;
+  console.log(targetScore);
+  blue.value = Math.floor(Math.random() * 12) + 1;
+  console.log(blue);
+  yellow.value = Math.floor(Math.random() * 12) + 1;
+  console.log(yellow);
+  green.value = Math.floor(Math.random() * 12) + 1;
+  console.log(green);
+  purple.value = Math.floor(Math.random() * 12) + 1;
+  console.log(purple);
+    $("targetScore").html(targetScore);
+    $("yourScore").html(currentScore);
+};
 
 
-    targetScore = Math.floor(Math.random() * 120) + 19;
 
-    crystal.blue.value = Math.floor(Math.random() * 12) + 1;
-    crystal.yellow.value = Math.floor(Math.random() * 12) + 1;
-    crystal.green.value = Math.floor(Math.random() * 12) + 1;
-    crystal.purple.value = Math.floor(Math.random() * 12) + 1;
-
-
-
-
-
-
-}
-
-
-
+startGame();
 
 //jQuery Functions
-
 $("#blue").click(function() {
-    alert("test");
-})
-$("#yellow").click(function () {
-    alert("test");
-})
-$("#green").click(function () {
-    alert("test");
-})
-$("#purple").click(function () {
-    alert("test");
-})
+  alert("test");
+});
+$("#yellow").click(function() {
+  alert("test");
+});
+$("#green").click(function() {
+  alert("test");
+});
+$("#purple").click(function() {
+  alert("test");
+});
